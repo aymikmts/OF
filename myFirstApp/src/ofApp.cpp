2fe291bfb40a79273a5da7715eeebc439473bbd8
aymikmts <rohy.2nd@gmail.com>
2017-11-27 17:55:04 +0900

    second commit.

--- myFirstApp/src/ofApp.cpp
+++ myFirstApp/src/ofApp.cpp
@@ -10,21 +10,47 @@ using namespace ofxCv;
 using namespace cv;
 
 void ofApp::setup() {
+    // init CV
     cam.initGrabber(ofGetWidth(), ofGetHeight());
     contourFinder.setMinAreaRadius(10);
     contourFinder.setMaxAreaRadius(200);
+    background.setLearningTime(900);
+    background.setThresholdValue(20);
+
+    // GUI
+    resetBackgroundButton.addListener(this, &ofApp::resetBackgroundPressed);
+    gui.setup();
+    gui.add(bgThresh.setup("background thresh", 50, 0, 100));
+    gui.add(contourThresh.setup("contour finder thresh", 127, 0, 255));
+    gui.add(resetBackgroundButton.setup("reset background"));
 }
 
 void ofApp::update() {
     cam.update();
     if (cam.isFrameNew()) {
-        contourFinder.setThreshold(ofMap(mouseX, 0, ofGetWidth(), 0, 255));
-        contourFinder.findContours(cam);
+        // ”wŒi·•ª‰æ‘œ‚ğ¶¬
+        background.setThresholdValue(bgThresh);
+        background.update(cam, thresholded);
+        thresholded.update();
+
+        // ”wŒi·•ª‚Ì—ÖŠs’Šo
+        contourFinder.setThreshold(contourThresh);
+        contourFinder.findContours(thresholded);
     }
 }
 
 void ofApp::draw() {
+    // ·•ª‰æ‘œ‚ğ•`‰æ
     ofSetColor(255);
-    cam.draw(0, 0);
+    thresholded.draw(0, 0);
+    // —ÖŠs’ŠoŒ‹‰Ê‚ğ•`‰æ
+    ofSetColor(255, 0, 0);
     contourFinder.draw();
+    // GUI
+    gui.draw();
+}
+
+
+void ofApp::resetBackgroundPressed() {
+    background.reset();
 }
--- myFirstApp/src/ofApp.h
+++ myFirstApp/src/ofApp.h
@@ -8,13 +8,22 @@
 
 #include "ofMain.h"
 #include "ofxCv.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp {
 public:
 	void setup();
 	void update();
 	void draw();
+    void resetBackgroundPressed();
     
     ofVideoGrabber cam;
     ofxCv::ContourFinder contourFinder;
+    ofxCv::RunningBackground background;
+    ofImage thresholded;
+
+    ofxPanel gui;
+    ofxFloatSlider bgThresh;            // 背景差分の閾値
+    ofxFloatSlider contourThresh;       // 輪郭抽出の閾値
+    ofxButton resetBackgroundButton;    // 背景リセットボタン
 };
